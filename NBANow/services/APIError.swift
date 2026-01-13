4d7fa22440ac869d0591f12434f8f357881ff5b2
GerardJalon <gerard.jalon@gmail.com>
2026-01-13 20:00:17 +0100

    feat: add content to API Error

--- NBANow/services/APIError.swift
+++ NBANow/services/APIError.swift
@@ -1,8 +1,16 @@
-//
-//  APIError.swift
-//  NBANow
-//
-//  Created by Gerard Jalon on 13/1/26.
-//
-
 import Foundation
+
+enum APIError: LocalizedError{ 
+    case invalidURL
+    case urlSessionError(Error) 
+    case invalidResponse
+    case decodingFailed(Error)
+    
+    var errorDescription: String?{
+        switch self{
+        case .invalidURL: return "The URL is invalid"
+        case .invalidResponse: return "Invalid response from server" 
+        case .urlSessionError(let error): return "Network error \(error.localizedDescription)"
+        case .decodingFailed(let error): return "Failed to decode response \(error.localizedDescription)"
+        }
+} }
